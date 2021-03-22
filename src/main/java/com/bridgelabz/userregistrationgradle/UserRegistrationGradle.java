264cb9ed80d0b34abf7c2dcc7c91a570460ad83e
MasterX01 <aakashsaxena14@gmail.com>
2021-03-22 15:19:10 +0530

    [Akash] ADD. added UC-1 working code

--- src/main/java/com/bridgelabz/userregistrationgradle/UserRegistrationGradle.java
+++ src/main/java/com/bridgelabz/userregistrationgradle/UserRegistrationGradle.java
@@ -1,7 +1,23 @@
 package com.bridgelabz.userregistrationgradle;
 
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class UserRegistrationGradle {
+
+    public static void firstName() {
+        Scanner scan = new Scanner(System.in);
+        String firstNameExp = "^[A-Z]{1}+[a-z]{2,}";
+        System.out.print("Please enter you first name: ");
+        String name = scan.next();
+        if(Pattern.matches(firstNameExp, name))
+            System.out.println("First Name you Entered is Valid");
+        else
+            System.out.println("First Name you entered is Invalid");
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to User Registration Program");
+        firstName();
     }
 }
