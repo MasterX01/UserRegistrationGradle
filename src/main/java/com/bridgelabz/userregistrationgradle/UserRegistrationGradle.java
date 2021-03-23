6d4705669d48977e9aa30c16eee8c53230270997
MasterX01 <aakashsaxena14@gmail.com>
2021-03-23 20:59:10 +0530

    [Akash] ADD. working UC-10 code

--- src/main/java/com/bridgelabz/userregistrationgradle/UserRegistrationGradle.java
+++ src/main/java/com/bridgelabz/userregistrationgradle/UserRegistrationGradle.java
@@ -5,69 +5,78 @@ import java.util.regex.Pattern;
 
 public class UserRegistrationGradle {
 
-    public static void firstName() {
-        Scanner scan = new Scanner(System.in);
+    public String validateFirstName(String fname) {
         String firstNameExp = "^[A-Z]{1}+[a-z]{2,}";
-        System.out.print("Please enter you first name: ");
-        String name = scan.next();
-        if(Pattern.matches(firstNameExp, name))
-            System.out.println("First Name you Entered is Valid");
+        if(Pattern.matches(firstNameExp, fname))
+            return "HAPPY";
         else
-            System.out.println("First Name you entered is Invalid");
+            return "SAD";
     }
 
-    public static void lastName() {
-        Scanner scan = new Scanner(System.in);
+    public String validateLastName(String lname) {
         String lastNameExp = "^[A-Z]{1}+[a-z]{2,}";
-        System.out.print("Please enter you last name: ");
-        String name = scan.next();
-        if(Pattern.matches(lastNameExp, name))
-            System.out.println("Last Name you Entered is Valid");
+        if(Pattern.matches(lastNameExp, lname))
+            return "HAPPY";
         else
-            System.out.println("Last Name you entered is Invalid");
+            return "SAD";
     }
 
-    public static void email() {
+    public String validateEmail(String email) {
+        String emailExp = "^([a-z0-9]+[-._+]?[a-zA-Z0-9]+)+@[a-z0-9]+.[a-z]{2,3}.[a-z]{2,3}$";
+        if(Pattern.matches(emailExp, email))
+            return "HAPPY";
+        else
+            return "SAD";
+    }
+
+    public String validateMobNum(String phone) {
+        String phoneExp = "[0-9]{2}\\s[0-9]{10}";
+        if(Pattern.matches(phoneExp, phone))
+            return "HAPPY";
+        else
+            return "SAD";
+    }
+
+    public String validatePassword(String password) {
+        String passwordExp = "^.*(?=.*[A-Z])(?=.*[0-9])([a-z])(?=.*[@#$%^&+=])(?=.{8,}).*$";
+        if(Pattern.matches(passwordExp, password))
+            return "HAPPY";
+        else
+            return "SAD";
+    }
+
+    public void takeInput() {
         Scanner scan = new Scanner(System.in);
-        String emailExp = "^[a-zA-Z0-9]+([.+_-]{0,1}+[a-zA-Z0-9])*+[@]+[a-zA-Z]+[.]+[a-zA-Z]{2,4}+([.]{1}+[a-zA-Z]{0,2})*";
+        // First Name
+        System.out.print("Please enter you first name: ");
+        String fname = scan.next();
+        validateFirstName(fname);
+
+        // Last Name
+        System.out.print("Please enter you last name: ");
+        String lname = scan.next();
+        validateLastName(lname);
+
+        // Email
         System.out.print("Please enter you Email: ");
         String email = scan.next();
-        if(Pattern.matches(emailExp, email))
-            System.out.println("The Email ID you Entered is Valid");
-        else
-            System.out.println("The Email ID you entered is Invalid");
-    }
+        validateEmail(email);
 
-    public static void mobNum() {
-        Scanner scan = new Scanner(System.in);
-        String phoneExp = "[0-9]{2}\\s[0-9]{10}";
+        // Phone Number
         System.out.print("Please enter you Phone Number: ");
         String phone = scan.nextLine();
-        System.out.println(phone);
-        if(Pattern.matches(phoneExp, phone))
-            System.out.println("The Phone Number you Entered is Valid");
-        else
-            System.out.println("The Phone Number you entered is Invalid");
-    }
+        validateMobNum(phone);
 
-    public  static void password() {
-        Scanner scan = new Scanner(System.in);
-        String passwordExp = "^.*(?=.*[A-Z])(?=.*[0-9])([a-z])(?=[@#$%^&+=]*)(?=.{8,}).*$";
+        // Password
         System.out.print("Please enter your Password of choice: ");
         String password = scan.nextLine();
-        System.out.println(password);
-        if(Pattern.matches(passwordExp, password))
-            System.out.println("The Password you Entered is Valid");
-        else
-            System.out.println("The Password you entered is Invalid");
+        validatePassword(password);
+
     }
 
     public static void main(String[] args) {
         System.out.println("Welcome to User Registration Program");
-        firstName();
-        lastName();
-        email();
-        mobNum();
-        password();
+        UserRegistrationGradle obj = new UserRegistrationGradle();
+        obj.takeInput();
     }
 }
